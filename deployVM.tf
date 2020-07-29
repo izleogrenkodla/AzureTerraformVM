ea82b35331e4a78385456aae6d9927d50b47def5
izleogrenkodla <izleogrenkodla@gmail.com>
2020-07-29 05:27:41 +0200

    Virtual Network Created

--- deployVM.tf
+++ deployVM.tf
@@ -1,3 +1,4 @@
+# Create Azure connection and resource group
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
@@ -11,4 +12,16 @@ resource "azurerm_resource_group" "myterraformgroup" {
         tags = {
             environment = "Terraform Demo"
         }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "francecentral"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
